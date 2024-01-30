d8665a22f727abe835b81462fa7a0db799946a82
Orobinsk <andy_prikol@mail.ru>
2024-01-30 23:11:03 +0300

    add RAWG api

--- src/entities/Deal/ui/DealCard/DealBigCard.tsx
+++ src/entities/Deal/ui/DealCard/DealBigCard.tsx
@@ -1,5 +1,6 @@
-import { FC, useState } from "react"
-import { IDeal, fetchLogo } from "shared/api"
+import { FC, memo, useEffect, useState } from "react"
+import { IDeal } from "shared/api"
+import { getRAWGSearchData } from "shared/api/model/api"
 import SalesTag from 'shared/assets/icons/sales-tag.svg'
 import { converDate } from "shared/lib/convertDate/convertDate"
 
@@ -12,18 +13,62 @@ const DealBigCard: FC<DealType> = ({ data }) => {
     const [active, setActive] = useState(false)
     const { title, releaseDate, steamAppID, salePrice, normalPrice, savings, storeID, thumb } = data
     const date = converDate(releaseDate)
-    const [imageSrc, setImageSrc] = useState(steamAppID ? `https://cdn.cloudflare.steamstatic.com/steam/apps/${steamAppID}/capsule_616x353.jpg` : thumb);
+    const [imageSrc, setImageSrc] = useState('');
 
-    const handleImageError = () => {
-        setImageSrc(thumb);
-    };
+    function checkImage(url: string): Promise<boolean> {
+        return new Promise((resolve) => {
+            const img = new Image();
+            img.onload = function () {
+                resolve(true);
+            };
+            img.onerror = function () {
+                resolve(false);
+            };
+            img.src = url;
+        });
+    }
 
-    // fetchLogo(steamAppID)
+       const fetchRAWGImg = async(title:string) =>{
+         try {
+            const { data, status } = await getRAWGSearchData(title);
+            if (status === 404) {
+                setImageSrc(thumb);
+            } else if (status === 200) {
+              setImageSrc(data.results[0].background_image);
+            } 
+          } catch (error) {
+            setImageSrc(thumb);
+            console.log("Произошла ошибка:", error);
+          }
+      }
+       
+    
+    useEffect(() => {
+            //проверяем разные стим ссылки
+        if (steamAppID) {
+            Promise.all([
+                checkImage(`https://cdn.cloudflare.steamstatic.com/steam/apps/${steamAppID}/capsule_616x353.jpg`),
+                checkImage(`https://cdn.cloudflare.steamstatic.com/steam/apps/${steamAppID}/header_586x192.jpg`),
+                checkImage(`https://cdn.akamai.steamstatic.com/steam/subs/${steamAppID}/header_586x192.jpg`) 
+            ]).then(([capsuleExists, headerExists, akamaiHeaderExist]) => {
+                if (capsuleExists){
+                    setImageSrc(`https://cdn.cloudflare.steamstatic.com/steam/apps/${steamAppID}/capsule_616x353.jpg`)
+                } else if (headerExists){
+                    setImageSrc(`https://cdn.cloudflare.steamstatic.com/steam/apps/${steamAppID}/header_586x192.jpg`)
+                } else if (akamaiHeaderExist){
+                    setImageSrc(`https://cdn.akamai.steamstatic.com/steam/subs/${steamAppID}/header_586x192.jpg`)
+                } else fetchRAWGImg(title.replace(/\s*-\s*/g, '-').toLowerCase());     
+            });
+        }
+         else fetchRAWGImg(title.replace(/\s*-\s*/g, '-').toLowerCase());
+    }, []);
+    
 
     return (
         <div onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)} className="rounded-2xl w-64 h-72 border border-secondary-text overflow-hidden cursor-pointer">
             <div className="max-h-56 overflow-hidden ">
-                <img src={imageSrc} alt={title} className={(active ? "brightness-75" : '') + ' transition-all duration-300 ease-in-out m-auto  object-cover w-full h-28 '} onError={handleImageError} />
+                <img src={imageSrc} alt={title} className={(active ? "brightness-75" : '') + ' transition-all duration-300 ease-in-out m-auto  object-cover w-full h-28 '}
+                 />
             </div>
             <div className="p-4 flex flex-col h-44">
                 <p className={(active ? 'text-primary' : 'text-primary-text') + " font-bold transition-color duration-300 ease-in-out"}>{title}</p>
@@ -39,7 +84,6 @@ const DealBigCard: FC<DealType> = ({ data }) => {
                     </div>
                     <div className="grid gap-2 grid-flow-col justify-start mt-2">
                         <div className="bg-primary w-0.5 h-4"></div>
-                        {/* <img  /> */}
                         <p className="text-secondary-text leading-none">Steam</p>
                     </div>
                 </div>
--- src/shared/api/model/base.ts
+++ src/shared/api/model/base.ts
@@ -1,9 +1,13 @@
-import axios from "axios";
-import { API_DEALS_URL, API_IMG_URL } from "shared/config/envConfig/envConfig";
+import axios from 'axios'
+import {
+  API_DEALS_URL,
+  API_STEAM_IMG_URL,
+} from 'shared/config/envConfig/envConfig'
 
 export const apiDeals = axios.create({
-    baseURL: API_DEALS_URL
+  baseURL: API_DEALS_URL,
 })
 export const apiImg = axios.create({
-    baseURL: API_IMG_URL
+  baseURL: API_STEAM_IMG_URL,
 })
+export const apiRAWG = axios.create({})
