03968038f600bebb6c53a8e5409171c971579e3d
Orobinsk <andy_prikol@mail.ru>
2024-02-12 22:56:18 +0300

    add DealCard.module.scss

--- src/entities/Deal/ui/DealCard/DealCard.tsx
+++ src/entities/Deal/ui/DealCard/DealCard.tsx
@@ -4,6 +4,8 @@ import { IDeal } from "shared/api"
 import SalesTag from 'shared/assets/icons/sales-tag.svg'
 import { storesInfo } from "shared/constants/storesInfo"
 import { converDate } from "shared/lib/convertDate/convertDate"
+import classes from './DealCard.module.scss'
+import classNames from "classnames"
 
 
 interface DealType {
@@ -34,26 +36,26 @@ const DealCard: FC<DealType> = ({ data }) => {
       }, []);
 
     return (
-        <div onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)} className="rounded-2xl w-64 h-72 border border-secondary-text overflow-hidden cursor-pointer">
-            <div className="max-h-56 overflow-hidden ">
-                <img src={imageSrc} alt={''} className={(active ? "brightness-75" : '') + ' transition-all duration-300 ease-in-out m-auto  object-cover w-full h-28 '}
+        <div onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)} className={classes.wrapper}>
+            <div className={classes.img_wrap}>
+                <img src={imageSrc} alt={''} className={classNames(classes.img, (active && classes.img_active))}
                  />
             </div>
-            <div className="p-4 flex flex-col h-44">
-                <p className={(active ? 'text-primary' : 'text-primary-text') + " font-bold transition-color duration-300 ease-in-out"}>{title.length<70 ? title : `${title.slice(0,70)}...`}</p>
-                <p className="text-secondary-text text-xs">{date}</p>
-                <div className="mb-0 mt-auto">
-                    <div className=" grid gap-2 grid-flow-col justify-start mt-3">
-                        <p className="text-primary-text font-bold">${salePrice}</p>
-                        <p className="text-secondary-text line-through text-xs">${normalPrice}</p>
-                        <div className="flex">
-                            <img src={SalesTag} className="h-4" />
-                            <div className="bg-primary text-white rounded-r-sm text-xs h-4 px-1">-{Math.round(Number(savings))}%</div>
+            <div className={classes.card_body}>
+                <p className={classNames(classes.title, (active && classes.title_active)) }>{title.length<70 ? title : `${title.slice(0,70)}...`}</p>
+                <p className={classes.date_release}>{date}</p>
+                <div className={classes.card_footer}>
+                    <div className={classes.price_wrapper}>
+                        <p className={classes.sale_price}>${salePrice}</p>
+                        <p className={classes.normal_price}>${normalPrice}</p>
+                        <div className={classes.sales_tag}>
+                            <img src={SalesTag} className={classes.img_sales_tag} />
+                            <div className={classes.sales_tag_savings}>-{Math.round(Number(savings))}%</div>
                         </div>
                     </div>
-                    <div className="grid gap-2 grid-flow-col justify-start mt-2">
-                        <div className="bg-primary w-0.5 h-4"></div>
-                        <p className="text-secondary-text leading-none">{storesInfo[Number(storeID)].storeName}</p>
+                    <div className={classes.store_wrapper}>
+                        <div className={classes.store_line}/>
+                        <p className={classes.store_title}>{storesInfo[Number(storeID)].storeName}</p>
                     </div>
                 </div>
             </div>
